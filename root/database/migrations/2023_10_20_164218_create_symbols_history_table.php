31f1285b394e71d69df34fa31c072c85e4950b5c
africz <attila.fricz@gmail.com>
2023-10-20 19:04:52 +0200

    dbstructure first attempt

---
+++ root/database/migrations/2023_10_20_164218_create_symbols_history_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('symbols_history', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('symbol_id');
+            $table->float('open');
+            $table->float('high');
+            $table->float('low');
+            $table->float('close');
+            $table->integer('volume');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('symbols_history');
+    }
+};
